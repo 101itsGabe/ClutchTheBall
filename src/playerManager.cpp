6697d812a4415090a154d8015d897870edb01d0c
101itsGabe <105684932+101itsGabe@users.noreply.github.com>
2023-09-25 11:31:35 -0400

    Fixing PlayerManager

--- src/playerManager.cpp
+++ src/playerManager.cpp
@@ -12,12 +12,28 @@ PlayerManager::PlayerManager()
 
 PlayerManager::PlayerManager(int numPlayers)
 {
-    playerSheet = IMG_Load("images/pngsheet.png");
-    SDL_FreeSurface(playerSheet);
+    playerSheet = IMG_Load("src/images/pngsheet.png");
+    if (playerSheet == NULL)
+    {
+        cout << "player sheet is null :(" << endl;
+    }
+    else
+    {
+        cout << "Player sheet IS NOT NULL" << endl;
+    }
+
     for (int i = 0; i < numPlayers; i++)
     {
         Player p;
         playerList.emplace_back(p);
+    }
+}
+PlayerManager::~PlayerManager()
+{
+    if (playerSheet != nullptr)
+    {
+        SDL_FreeSurface(playerSheet);
+        playerSheet = nullptr; // Optional: Set to nullptr to avoid potential issues with double freeing
     }
 }
 
@@ -28,36 +44,48 @@ vector<Player> &PlayerManager::getPlayerList()
 
 SDL_Surface *PlayerManager::getSurface()
 {
-    SDL_Surface *copy = playerSheet;
-    SDL_FreeSurface(copy);
-    return copy;
+    return playerSheet;
 }
 
 void PlayerManager::RenderPlayers(SDL_Renderer *renderer, vector<Tile> tiles)
 {
-    for (Player &p : playerList)
+    SDL_Surface *copy = getSurface();
+    cout << "COPY I TOOK YOUR BITCH AND BODY" << endl;
+    if (!getPlayerList().empty())
     {
-        Tile tile = tiles[p.GetTile()];
-        int padding = tile.getTileData().tileH * 0.05;
-        int posX = tile.getTileData().tileX + padding;
-        int posY = tile.getTileData().tileY + padding;
-        int W = tile.getTileData().tileW - 2 * padding;
-        int H = tile.getTileData().tileH - 2 * padding;
-
-        SDL_Rect pngSheetRect = {125, 25, 58, 94};
-        SDL_Rect destplayerRect = {posX, posY, W, H};
-
-        if (p.getTeam() == "Team1")
-            SDL_SetRenderDrawColor(renderer, 250, 0, 0, 250);
-        else if (p.getTeam() == "Team2")
+        for (Player &p : getPlayerList())
         {
-            SDL_SetRenderDrawColor(renderer, 0, 0, 250, 250);
-            // cout << getTeam();
-        }
+            Tile tile = tiles[p.GetTile()];
+            int padding = tile.getTileData().tileH * 0.05;
+            int posX = tile.getTileData().tileX + padding;
+            int posY = tile.getTileData().tileY + padding;
+            int W = tile.getTileData().tileW - 2 * padding;
+            int H = tile.getTileData().tileH - 2 * padding;
 
-        SDL_Texture *t = SDL_CreateTextureFromSurface(renderer, getSurface());
-        if (t != NULL)
-            SDL_RenderCopy(renderer, t, &pngSheetRect, &destplayerRect);
-        SDL_DestroyTexture(t);
+            SDL_Rect pngSheetRect = {125, 25, 58, 94};
+            SDL_Rect destplayerRect = {posX, posY, W, H};
+
+            if (p.getTeam() == "Team1")
+                SDL_SetRenderDrawColor(renderer, 250, 0, 0, 250);
+            else if (p.getTeam() == "Team2")
+            {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 250, 250);
+            }
+            if (copy != nullptr)
+            {
+                SDL_Texture *t = SDL_CreateTextureFromSurface(renderer, copy);
+                if (t != nullptr)
+                {
+                    cout << "inside before render copy" << endl;
+                    SDL_RenderCopy(renderer, t, &pngSheetRect, &destplayerRect);
+                }
+                else
+                {
+                    cout << "Well well well texture IS null" << endl;
+                }
+                SDL_DestroyTexture(t);
+            }
+        }
+        SDL_FreeSurface(copy);
     }
 }
--- src/playerManager.h
+++ src/playerManager.h
@@ -12,6 +12,7 @@ private:
 
 public:
     PlayerManager();
+    ~PlayerManager();
     PlayerManager(int);
 
     // void addPlayer(Player );
